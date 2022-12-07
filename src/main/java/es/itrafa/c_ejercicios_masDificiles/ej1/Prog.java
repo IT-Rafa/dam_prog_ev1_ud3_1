3534b56bb256e0fae5cf7e89a20af85c92598a97
it-rafa <it-rafamartinez@outlook.es>
2022-12-07 20:29:57 +0100

    Ejer 1 2

--- src/main/java/es/itrafa/c_ejercicios_masDificiles/ej1/Prog.java
+++ src/main/java/es/itrafa/c_ejercicios_masDificiles/ej1/Prog.java
@@ -45,6 +45,39 @@ package es.itrafa.c_ejercicios_masDificiles.ej1;
  * 2
  * 41
  * 41
+ *
+ * <p>
+ * Entrada
+ * <p>
+ * 49 1
+ * 1 49
+ * 1 50
+ * 50 1
+ * 51 1
+ * 1 51
+ * 48 1
+ * 1 48
+ * 99 1
+ * 1 99
+ * 99 2
+ * 2 99
+ * 0 0
+ *
+ * Resultado
+ *
+ * 48
+ * 48
+ * 49
+ * 49
+ * 49
+ * 49
+ * 47
+ * 47
+ * 1
+ * 1
+ * 2
+ * 2
+
  */
 
 import java.util.Scanner;
@@ -56,12 +89,42 @@ public class Prog {
         int primero = sc.nextInt();
         int segundo = sc.nextInt();
 
+
         while (primero != 0 || segundo != 0) {
+            // Controla secuencia de ambos
+            int ruta = 0;
 
+            // ini es indice para la cuenta hacia atrás
+            int ini = primero;
+            // i controla la cuenta adelante. internamente la cuenta atrás
+            // el bloqueo normal se hace al llegar al número hacia adelante;
+            // El bloqueo se hace como "no es igual" porque puede estar más atrás.
+            for (int i = primero; i != segundo; i++){
+                // Si cuenta atrás lo encuentra antes sale
+                if(ini == segundo){
+                    break;
+                }
 
+                // controlamos i al llegar al 100 (vueltas todo el rato, hasta encontrar número
+                if(i == 99) {
+                    i = 0;
+                }
+                // controlamos índice cuenta atrás hasta el 0.
+                if(ini == 1){
+                    ini = 100;
+                }
+                // bajamos 1 el índice cuenta atrás
+                ini--;
+                // contamos cada paso
+                ruta++;
+
+            }
+            // Mostramos salida
+            System.out.println(ruta);
+
+            // Repetimos hasta 0 0
             primero = sc.nextInt();
             segundo = sc.nextInt();
         }
     }
-
 }
--- src/main/java/es/itrafa/c_ejercicios_masDificiles/ej2/Prog.java
+++ src/main/java/es/itrafa/c_ejercicios_masDificiles/ej2/Prog.java
@@ -1,54 +1,66 @@
 package es.itrafa.c_ejercicios_masDificiles.ej2;
 
+import java.util.Scanner;
+
 /**
  * [fuente: aceptaelreto.com (124)]
- *
  * [cat: bucles simples]
- *
  * CUANTAS ME LLEVO
- *
+ * <p>
  * Cuando aprendemos a sumar números pronto nos cuentan aquello de
  * "llevarse una": cuando los dos dígitos que sumamos llegan a la
  * decena tenemos "acarreo" que debemos sumar a los siguientes
  * dígitos (de la izquierda).
- *
+ * <p>
  * Cuando nuestros maestros nos ponían ejercicios, antes tenían que
- * contar cuántas veces tendríamos que "llevarnos una" y en base a
- * eso medían la dificultad del ejercicio.
- *
+ * contar cuántas veces tendríamos que "llevarnos una" y, con base en
+ * eso, medían la dificultad del ejercicio.
+ * <p>
  * ¿Puedes hacer un programa que automatice esa tarea?
- *
+ * <p>
  * Entrada
- *
+ * <p>
  * La entrada estará compuesta de numerosos casos de prueba, cada uno
- * de ellos en una línea. En cada línea apareceran dos números positivos
+ * de ellos en una línea. En cada línea aparecerían dos números positivos
  * separados por un espacio. Se garantiza que los números no tendrán más
  * de 1000 dígitos.
- *
- * Los casos de prueba terminan con el caso especial 0 0, que no
+ * <p>
+ * Los casos de prueba terminan con el caso especial 0 0, que no
  * provocará salida.
- *
+ * <p>
  * Salida
- *
+ * <p>
  * Para cada caso de prueba hay que escribir en una línea el número de
  * veces que hay "acarreo" en la suma.
- *
+ * <p>
  * Por ejemplo:
  * Entrada
- *
+ * <p>
  * 123 456
  * 555 555
  * 123 594
  * 0 0
- *
+ * <p>
  * Resultado
- *
+ * <p>
  * 0
  * 3
  * 1
  */
 public class Prog {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] numbs = sc.nextLine().split(" ");
 
+        while (!numbs[0].equals("0") || !numbs[1].equals("0")) {
+            int a = Integer.parseInt(numbs[0]);
+            int b = Integer.parseInt(numbs[1]);
+            for (int i= 0; i < numbs[0].length(); i++){
+                System.out.println(a%10);
+                a = a - (a%10) - 10;
+            }
+
+            numbs = sc.nextLine().split(" ");
+        }
     }
 }
